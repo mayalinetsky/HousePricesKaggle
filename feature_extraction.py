3ab7254053c54c967e79d55d60eb9b896aacaafd
Maya Linetsky <maya.linetsky@gmail.com>
2024-02-14 13:32:01 +0200

    Implementing extractor of relative stats
    This implementation includes calculating the 'SizeRelativeToMedian' and 'IsInTopQuartile' based on the train set

--- feature_extraction.py
+++ feature_extraction.py
@@ -4,22 +4,23 @@ It gets raw data and transforms it into features ready for preprocessing and lab
 """
 from typing import *
 
+import numpy as np
 import pandas as pd
-from sklearn.base import TransformerMixin
+from sklearn.base import TransformerMixin, BaseEstimator
 from constants import *
 
 
-class FeatureExtractor(TransformerMixin):
+class FeatureExtractor(BaseEstimator, TransformerMixin):
     """
     Base class for transforming raw data into new features.
 
     For example: one-hot encoding
     """
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self):
+        super().__init__()
 
     def fit(self, x, y=None):
-        return x
+        return self
 
     def transform(self, x):
         return x
@@ -44,3 +45,30 @@ def join_porch_areas(x: pd.DataFrame):
     x.drop(columns=close_porches + open_porches, inplace=True)
     return x
 
+
+class RelativeFeatureExtractor(FeatureExtractor):
+    """
+    Class for adding new features that are base on the relative position of the old ones.
+
+    New features:
+    - SizeRelativeToMedian TODO explain
+    - IsInTopQuartile TODO explain
+    """
+    def __init__(self):
+        super().__init__()
+        self._neighborhood_stats = None
+
+    def fit(self, X: pd.DataFrame, y=None):
+        self._neighborhood_stats = X.groupby(Neighborhood)[GrLivArea].agg(
+            ['median', lambda x: np.quantile(x, 0.75)])
+        self._neighborhood_stats.columns = ['50%', '75%']
+        self._neighborhood_stats.reset_index(inplace=True)
+
+        return super().fit(X, y)
+
+    def transform(self, X: pd.DataFrame):
+        new_X = X.copy()
+        X_with_stats = pd.merge(X, self._neighborhood_stats, on=Neighborhood, how='left').set_index(X.index)
+        new_X['SizeRelativeToMedian'] = X_with_stats[GrLivArea] / X_with_stats['50%']
+        new_X['IsInTopQuartile'] = (X_with_stats[GrLivArea] > X_with_stats['75%']).astype(int)
+        return new_X
